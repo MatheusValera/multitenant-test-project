3080d503995a6e49bd48c7caa463080cd9ead9eb
MatheusValera <lrfp1000@gmail.com>
2025-03-19 15:37:09 -0300

    refactor: deleted all

--- backend/src/domain/repositories/ITenantRepository.ts
+++
@@ -1,7 +0,0 @@
-import { type Tenant } from '../entities/Tenant'
-
-export interface ITenantRepository {
-  create: (tenant: Tenant) => Promise<Tenant>
-  findById: (id: string) => Promise<Tenant | null>
-  // Adicione outros métodos conforme necessário
-}
--- backend/src/presentation/routes/tenantRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { TenantController } from '../controllers/TenantController'
-
-const router = Router()
-
-// eslint-disable-next-line @typescript-eslint/unbound-method
-router.post('/tenants', TenantController.create)
-
-export default router
